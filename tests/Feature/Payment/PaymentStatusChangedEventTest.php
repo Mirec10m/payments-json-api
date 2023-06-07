a2610b4f1602044d86d6b7da0d3b06f5e61ce4ee
Miroslav Grofčík <miroslav.grofcik@demi.sk>
2023-06-07 20:50:32 +0200

    Add payment status changed event test (tests)

---
+++ tests/Feature/Payment/PaymentStatusChangedEventTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature\Payment;
+
+use App\Enums\PaymentStatusEnum;
+use App\Events\PaymentStatusChangedEvent;
+use App\Http\Middleware\ValidateSignature;
+use App\Models\Payment;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\URL;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class PaymentStatusChangedEventTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test if PaymentStatusChangedEvent can be dispatched when Payment status changes.
+     *
+     * @return void
+     */
+    public function test_event_is_dispatched_when_status_changes(): void
+    {
+        Event::fake();
+
+        $user = User::factory()->create();
+        $payment = Payment::factory()->create([
+            'status' => PaymentStatusEnum::NEW->value,
+            'expired_at' => Carbon::tomorrow(),
+        ]);
+
+        Sanctum::actingAs($user);
+
+        $response = $this->withoutMiddleware(ValidateSignature::class)
+            ->get('/api/payments/check/' . $payment->id);
+
+        Event::assertDispatched(PaymentStatusChangedEvent::class);
+
+        $response->assertStatus(200);
+    }
+}
